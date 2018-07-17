f954b11316584e6c927cfe48cb6428f708ea4f25
Paul Wright <pwright@redhat.com>
2018-07-17 11:46:39 +0100

    update

--- bin/ant-include.sh
+++ bin/ant-include.sh
@@ -3,14 +3,12 @@
 # until antora includes a feature that enables allow-uri-read for asciidoctor
 # need to run this script to populate content from other repos
 
-wget https://raw.githubusercontent.com/asciidoctor/asciidoctor-extensions-lab/master/scripts/asciidoc-coalescer.rb
 
 find modules -type f -name '*.inc' |\
   while IFS= read -r inc_path
   do
     inc_dirname=$(dirname "${inc_path}")
-    ruby asciidoc-coalescer.rb  -a allow-uri-read $inc_path > $inc_path-rantora.adoc
+    ruby ./bin/asciidoc-coalescer.rb  -a allow-uri-read $inc_path > $inc_path-rantora.adoc
     
   done
 
-rm asciidoc-coalescer.rb
